9108e077b658908c56ad267a480d86823eaaa75b
Ericsson Almeida <101663642+Ralgnafar@users.noreply.github.com>
2023-07-20 11:04:41 -0300

    Update Exercicio_11.py

--- Aula 5/Exercicio_11.py
+++ Aula 5/Exercicio_11.py
@@ -3,4 +3,4 @@ n = int(input(" Digite um número inteiro: "))
 soma = 0
 for i in range(1, n + 1):
     soma += i
-print(" A soma de todos os números de 1 a", n, "é", soma)
+print(" A soma dos números de 1 a", n, "é", soma)
